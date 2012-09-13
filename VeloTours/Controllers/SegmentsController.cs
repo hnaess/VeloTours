7794c00079236d516a40714269952e2c1f937c4e
hnaess <hnaess@hotmail.com>
2012-09-13 21:00:05 +0200

    Initial

---
+++ VeloTours/Controllers/SegmentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VeloTours.Controllers
+{
+    public class SegmentsController : Controller
+    {
+        //
+        // GET: /Segments/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
