7cdeac9a3fb48bddd2606c876c213605e044645f
hnaess <hnaess@hotmail.com>
2012-09-21 13:49:53 +0200

    Added RestSharp

---
+++ VeloTours/Controllers/Pages/SegmentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VeloTours.Models;
+using VeloTours.ViewModel;
+
+namespace VeloTours.Controllers.Pages
+{
+    public class SegmentController : Controller
+    {
+        private TourModelContainer db = new TourModelContainer();
+
+        public String Index(int id)
+        {
+            return "TODO" + id;
+        }
+
+        public ActionResult Segment(int id)
+        {
+            Segment segment = db.Segments.Find(id);
+            return View(segment);
+        }
+
+        public ActionResult Area(int id)
+        {
+            SegmentArea segmentArea = db.SegmentAreas.Find(id);
+            return View(segmentArea);
+        }
+
+        // <iframe allowtransparency="true" frameborder="0" height="405" scrolling="no" src="http://app.strava.com/segments/632847/embed" width="590"></iframe>
+    }
+}
---
+++ VeloTours/Helpers/HtmlHelpers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace VeloTours.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static string SimpleLink(this HtmlHelper html, string url, string text)
+        {
+            return String.Format("<a href=\"{0}\">{1}</a>", url, text);
+        }
+
+        public static MvcHtmlString HtmlLink(this HtmlHelper html, string url, string text, object htmlAttributes)
+        {
+            TagBuilder tb = new TagBuilder("a");
+            tb.InnerHtml = text;
+            tb.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+            tb.MergeAttribute("href", url);
+            return MvcHtmlString.Create(tb.ToString(TagRenderMode.Normal));
+        }
+
+        public static String Truncate(this HtmlHelper helper, string input, int length)
+        {
+            if (input.Length <= length)
+            {
+                return input;
+            }
+            else
+            {
+                return input.Substring(0, length) + "...";
+            }
+        }
+    }
+}
