0009bc2ea3648c56f0709d7c9859920258d4a627
Henrik <hnaess@hotmail.com>
2012-09-29 20:35:50 +0200

    Updated libraries and added test project

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // http://www.geekzone.co.nz/VisualStudio/7906
+        // https://stravasite-main.pbworks.com/w/page/51754151/Strava%20REST%20API%20Method%3A%20segments%20show
+        // http://json2csharp.com/
+
+        
+        static void Main(string[] args)
+        {
+            System.Net.WebClient proxy = new System.Net.WebClient();
+            string uri = "http://app.strava.com/api/v1/segments/1637189";
+
+            byte[] resultJsonAsString = proxy.DownloadData(new Uri(uri));
+            string result = System.Text.Encoding.UTF8.GetString(resultJsonAsString);
+
+            JObject jResult = JObject.Parse(result);
+            IList<JToken> jResults = jResult["segment"].Children().ToList();
+
+            List<ConsoleApplication1.StravaAPI.Segment> stravaSegments = new List<ConsoleApplication1.StravaAPI.Segment>();
+            //foreach(JToken stravaSegment in stravaSegments)
+            //{
+                
+            //}
+        }
+    }
+}
---
+++ ConsoleApplication1/StravaAPI/Segment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.StravaAPI
+{
+    public class Segment
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double distance { get; set; }
+        public double elevationGain { get; set; }
+        public double elevationHigh { get; set; }
+        public double elevationLow { get; set; }
+        public double averageGrade { get; set; }
+        public string climbCategory { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Segment segment { get; set; }
+    }
+}
