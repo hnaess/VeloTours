d2fadb7e40da485f9bb013c1e85ba7866335fa27
Henrik <hnaess@hotmail.com>
2012-09-30 19:16:22 +0200

    Further work on testing API

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -17,17 +17,39 @@ namespace ConsoleApplication1
         // https://stravasite-main.pbworks.com/w/page/51754151/Strava%20REST%20API%20Method%3A%20segments%20show
         // http://json2csharp.com/
 
+
+        // http://app.strava.com/api/v1/segments/229781/efforts
+        // http://app.strava.com/api/v1/segments/229781/efforts?offset=2
+
         
         static void Main(string[] args)
         {
             var SegmentId = 1637189;
 
             StravaWebClient cli = new StravaWebClient();
+            SegmentService serv = new SegmentService(cli);
+            //Segment segment = serv.Show(SegmentId);
+            
+            int i = 0;
+            do
+            {
+                SegmentEfforts segmentEfforts = serv.Efforts(SegmentId);
+                PrintEffort(segmentEfforts, ref i);
+                serv.Index(serv.
 
-            //SegmentService serv = new SegmentService(web);
+            } while (i % 50 == 0);
+         }
 
-            //var s = serv.Efforts(SegmentId);
+        private static void PrintEffort(SegmentEfforts segmentEfforts, ref int i)
+        {
+            foreach (Effort effort in segmentEfforts.Efforts)
+            {
+                i++;
+                Athlete athlete = effort.Athlete;
+                Console.WriteLine(String.Format("{0}, {1}, {2}, {3}", i, effort.Athlete.Name, effort.ElapsedTime, effort.StartDate.ToString()));
+            }
         }
+
 
         void test1()
         {
